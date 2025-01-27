16f063150e25cae3d1dc884d822569ad6aad8666
Habib <habiburnabiarafat@gmail.com>
2025-01-27 18:58:13 +0600

    fix: shared folder removed

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 
 import "@workspace/ui/globals.css"
 import { Providers } from "@/components/providers"
+import { MainNavigation } from "@/components/main-nav"
 
 const fontSans = Geist({
   subsets: ["latin"],
@@ -22,7 +23,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
-      >
+      ><MainNavigation />
         <Providers>{children}</Providers>
       </body>
     </html>
